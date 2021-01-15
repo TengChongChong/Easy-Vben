c2169d316d3f10724e02ad8e93e13d27dad3e4b2
tengchong <26172583@qq.com>
2021-01-15 23:37:05 +0800

    添加示例模块

---
+++ easy-sample/src/main/java/com/easy/restful/sample/model/SampleWorkFlow.java
@@ -0,0 +1,190 @@
+package com.easy.restful.sample.model;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 流程示例
+ *
+ * @author TengChong
+ * @date 2020-04-26
+ */
+@TableName("sample_work_flow")
+public class SampleWorkFlow extends Model<SampleWorkFlow>{
+
+    @TableId(value = "id")
+    private String id;
+
+    /**
+     * 流程实例id
+     */
+    private String processInstanceId;
+
+    /**
+     * 请假类型
+     */
+    private String leaveType;
+
+    /**
+     * 开始时间
+     */
+    private Date startDate;
+
+    /**
+     * 结束时间
+     */
+    private Date endDate;
+
+    /**
+     * 原因
+     */
+    private String reason;
+
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private String createUser;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createDate;
+
+    /**
+     * 编辑人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String editUser;
+
+    /**
+     * 编辑时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date editDate;
+
+    /**
+     * 状态
+     */
+    private Integer status;
+
+    //
+
+    /**
+     * 日期范围
+     */
+    @TableField(exist=false)
+    private String dateRange;
+
+    /**
+     * 流程状态
+     */
+    @TableField(exist=false)
+    private String workFlowStatus;
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getProcessInstanceId() {
+        return processInstanceId;
+    }
+
+    public void setProcessInstanceId(String processInstanceId) {
+        this.processInstanceId = processInstanceId;
+    }
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+    public String getEditUser() {
+        return editUser;
+    }
+
+    public void setEditUser(String editUser) {
+        this.editUser = editUser;
+    }
+    public Date getEditDate() {
+        return editDate;
+    }
+
+    public void setEditDate(Date editDate) {
+        this.editDate = editDate;
+    }
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getDateRange() {
+        return dateRange;
+    }
+
+    public void setDateRange(String dateRange) {
+        this.dateRange = dateRange;
+    }
+
+    public String getWorkFlowStatus() {
+        return workFlowStatus;
+    }
+
+    public void setWorkFlowStatus(String workFlowStatus) {
+        this.workFlowStatus = workFlowStatus;
+    }
+}
---
+++ easy-sample/src/main/java/com/easy/restful/sample/service/SampleWorkFlowService.java
@@ -0,0 +1,53 @@
+package com.easy.restful.sample.service;
+
+
+import com.easy.restful.common.core.common.pagination.Page;
+import com.easy.restful.sample.model.SampleWorkFlow;
+
+/**
+ * 流程示例
+ *
+ * @author TengChong
+ * @date 2020-04-26
+ */
+public interface SampleWorkFlowService {
+    /**
+     * 列表
+     *
+     * @param object 查询条件
+     * @param page   分页
+     * @return 数据集合
+     */
+    Page<SampleWorkFlow> select(SampleWorkFlow object, Page<SampleWorkFlow> page);
+
+    /**
+     * 详情
+     *
+     * @param id id
+     * @return 详细信息
+     */
+    SampleWorkFlow input(String id);
+
+    /**
+     * 新增
+     *
+     * @return 默认值
+     */
+    SampleWorkFlow add();
+
+    /**
+     * 删除
+     *
+     * @param ids 数据ids
+     * @return 是否成功
+     */
+    boolean remove(String ids);
+
+    /**
+     * 保存
+     *
+     * @param object 表单内容
+     * @return 保存后信息
+     */
+    SampleWorkFlow saveData(SampleWorkFlow object);
+}
