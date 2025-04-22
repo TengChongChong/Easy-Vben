ddc9256eb52c5937a32e7ac822791991b507bffd
TengChongChong <26172583@qq.com>
2025-04-22 22:51:58 +0800

    fix: 修复代码生成问题

---
+++ easy-sample/src/main/java/com/easy/admin/sample/model/vo/SampleGeneralVO.java
@@ -0,0 +1,22 @@
+package com.easy.admin.sample.model.vo;
+
+import com.easy.admin.sample.model.SampleGeneral;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 代码生成示例
+ *
+ * @author 系统管理员
+ * @date 2025-03-21
+ */
+@Data
+public class SampleGeneralVO extends SampleGeneral implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 非表字段
+}
