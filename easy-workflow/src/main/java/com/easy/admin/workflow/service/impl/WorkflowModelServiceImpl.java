86bdf472c46371721adca458ffe66c6cf3c585f6
TengChongChong <26172583@qq.com>
2025-07-23 23:27:31 +0800

    feat: 添加工作流

--- easy-workflow/src/main/java/com/easy/admin/workflow/service/impl/WorkflowModelServiceImpl.java
+++ easy-workflow/src/main/java/com/easy/admin/workflow/service/impl/WorkflowModelServiceImpl.java
@@ -1,15 +1,10 @@
-package com.easy.admin.activiti.service.impl;
+package com.easy.admin.workflow.service.impl;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.easy.admin.activiti.constant.ActivitiModelConst;
-import com.easy.admin.activiti.constant.status.ActivitiSuspensionStatus;
-import com.easy.admin.activiti.dao.ActivitiModelMapper;
-import com.easy.admin.activiti.model.ActivitiModel;
-import com.easy.admin.activiti.service.ActivitiModelService;
 import com.easy.admin.common.core.common.pagination.Page;
 import com.easy.admin.common.core.constant.CommonConst;
 import com.easy.admin.common.core.exception.EasyException;
@@ -17,6 +12,11 @@ import com.easy.admin.config.sa.token.util.SessionUtil;
 import com.easy.admin.file.model.FileDownload;
 import com.easy.admin.file.service.FileDownloadService;
 import com.easy.admin.file.util.file.FileUtil;
+import com.easy.admin.workflow.constant.ActivitiModelConst;
+import com.easy.admin.workflow.constant.status.WorkflowSuspensionStatus;
+import com.easy.admin.workflow.dao.WorkflowModelMapper;
+import com.easy.admin.workflow.model.vo.WorkflowModelVO;
+import com.easy.admin.workflow.service.WorkflowModelService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -28,40 +28,34 @@ import org.activiti.editor.language.json.converter.BpmnJsonConverter;
 import org.activiti.engine.ActivitiException;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.persistence.entity.ModelEntity;
+import org.activiti.engine.impl.persistence.entity.ModelEntityImpl;
 import org.activiti.engine.repository.Deployment;
-import org.apache.batik.transcoder.TranscoderException;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.activiti.engine.repository.Model;
 import org.apache.commons.io.IOUtils;
 import org.dromara.x.file.storage.core.FileInfo;
-import org.dromara.x.file.storage.core.FileStorageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.activiti.editor.constants.ModelDataJsonConstants.MODEL_DESCRIPTION;
-import static org.activiti.editor.constants.ModelDataJsonConstants.MODEL_NAME;
-
 /**
- * Activiti 流程模型
- * 部分代码来源于 Activiti（StencilsetRestResource.java、ModelEditorJsonRestResource.java、ModelSaveRestResource.java）
+ * 流程模型
  *
  * @author TengChongChong
- * @date 2019-07-02
- */
+ * @date 2025-07-14
+ **/
 @Slf4j
 @Service
-public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, ActivitiModel> implements ActivitiModelService {
+public class WorkflowModelServiceImpl extends ServiceImpl<WorkflowModelMapper, WorkflowModelVO> implements WorkflowModelService {
 
     @Autowired
     private RepositoryService repositoryService;
@@ -72,24 +66,22 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
     @Autowired
     private ObjectMapper objectMapper;
 
-    /**
-     * 文件存储
-     */
-    @Autowired
-    private FileStorageService fileStorageService;
-
 
     @Override
-    public Page<ActivitiModel> select(ActivitiModel activitiModel, Page<ActivitiModel> page) {
-        QueryWrapper<ActivitiModel> queryWrapper = new QueryWrapper<>();
-        if (activitiModel != null) {
+    public Page<WorkflowModelVO> select(WorkflowModelVO workflowModel, Page<WorkflowModelVO> page) {
+        QueryWrapper<WorkflowModelVO> queryWrapper = new QueryWrapper<>();
+        if (workflowModel != null) {
             // 名称
-            if (StrUtil.isNotBlank(activitiModel.getName())) {
-                queryWrapper.like("arm.name_", activitiModel.getName());
+            if (StrUtil.isNotBlank(workflowModel.getName())) {
+                queryWrapper.like("arm.name_", workflowModel.getName());
             }
             // key
-            if (StrUtil.isNotBlank(activitiModel.getKey())) {
-                queryWrapper.like("arm.key_", activitiModel.getKey());
+            if (StrUtil.isNotBlank(workflowModel.getKey())) {
+                queryWrapper.like("arm.key_", workflowModel.getKey());
+            }
+            // category
+            if (StrUtil.isNotBlank(workflowModel.getCategory())) {
+                queryWrapper.eq("arm.category_", workflowModel.getCategory());
             }
         }
         page.setDefaultDesc("arm.create_time_");
@@ -143,42 +135,42 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
     }
 
     @Override
-    public ModelEntity saveData(ActivitiModel activitiModel) {
-        if (StrUtil.isBlank(activitiModel.getKey())) {
-            throw new EasyException("标识不能为空");
+    public ModelEntityImpl saveData(WorkflowModelVO workflowModel) {
+        if (StrUtil.isBlank(workflowModel.getKey())) {
+            throw new EasyException("流程标识不能为空");
         }
-        if (StrUtil.isBlank(activitiModel.getName())) {
-            throw new EasyException("名称不能为空");
+        if (StrUtil.isBlank(workflowModel.getName())) {
+            throw new EasyException("流程名称不能为空");
         }
-        if (StrUtil.isBlank(activitiModel.getId())) {
-            activitiModel.setId(null);
+        if (StrUtil.isBlank(workflowModel.getId())) {
+            workflowModel.setId(null);
         }
-        if (StrUtil.isBlank(activitiModel.getCategory())) {
-            activitiModel.setCategory(null);
+        if (StrUtil.isBlank(workflowModel.getCategory())) {
+            workflowModel.setCategory(null);
         }
-        if (checkKeyIsExistence(activitiModel.getKey(), activitiModel.getId())) {
-            throw new EasyException("标识已存在，请修改后重试");
+        if (checkKeyIsExistence(workflowModel.getKey(), workflowModel.getId())) {
+            throw new EasyException("流程标识已存在，请修改后重试");
         }
         //流程版本
         int revision = 1;
 
         ObjectNode modelNode = objectMapper.createObjectNode();
-        modelNode.put(ModelDataJsonConstants.MODEL_NAME, activitiModel.getName());
-        modelNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, activitiModel.getDescription());
+        modelNode.put(ModelDataJsonConstants.MODEL_NAME, workflowModel.getName());
+        modelNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, workflowModel.getDescription());
         modelNode.put(ModelDataJsonConstants.MODEL_REVISION, revision);
-        activitiModel.setMetaInfo(modelNode.toString());
+        workflowModel.setMetaInfo(modelNode.toString());
         // 保存模型信息
-        ModelEntity modelEntity = activitiModel.getModelEntity();
+        ModelEntityImpl modelEntity = workflowModel.getModelEntity();
         repositoryService.saveModel(modelEntity);
-        activitiModel.setId(modelEntity.getId());
-        setModelEditorSource(activitiModel);
+        workflowModel.setId(modelEntity.getId());
+        setModelEditorSource(workflowModel);
 
         return modelEntity;
     }
 
-    private void setModelEditorSource(ActivitiModel activitiModel) {
-        if (activitiModel.getModelFile() != null) {
-            byte[] bytes = FileUtil.getFileBytes(activitiModel.getModelFile());
+    private void setModelEditorSource(WorkflowModelVO workflowModel) {
+        if (workflowModel.getModelFile() != null) {
+            byte[] bytes = FileUtil.getFileBytes(workflowModel.getModelFile());
             InputStream bpmnStream = new ByteArrayInputStream(bytes);
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
             // 读取bpmn.xml
@@ -202,21 +194,21 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
 
             // 修改标识、名称、描述信息
             ObjectNode properties = objectMapper.createObjectNode();
-            properties.put(ActivitiModelConst.PROPERTIES_PROCESS_ID, activitiModel.getKey());
-            properties.put(ActivitiModelConst.NAME, activitiModel.getName());
+            properties.put(ActivitiModelConst.PROPERTIES_PROCESS_ID, workflowModel.getKey());
+            properties.put(ActivitiModelConst.NAME, workflowModel.getName());
             properties.put(ActivitiModelConst.PROPERTIES_PROCESS_AUTHOR, SessionUtil.getCurrentUser().getNickname());
-            properties.put(ActivitiModelConst.PROPERTIES_DOCUMENTATION, activitiModel.getDescription());
+            properties.put(ActivitiModelConst.PROPERTIES_DOCUMENTATION, workflowModel.getDescription());
             objectNode.set(ActivitiModelConst.PROPERTIES, properties);
-            repositoryService.addModelEditorSource(activitiModel.getId(), objectNode.toString().getBytes(StandardCharsets.UTF_8));
+            repositoryService.addModelEditorSource(workflowModel.getId(), objectNode.toString().getBytes(StandardCharsets.UTF_8));
         } else {
             // ModelEditorSource
-            String modelId = activitiModel.getId();
+            String modelId = workflowModel.getId();
             // 流程属性
             ObjectNode properties = objectMapper.createObjectNode();
-            properties.put(ActivitiModelConst.PROPERTIES_PROCESS_ID, activitiModel.getKey());
-            properties.put(ActivitiModelConst.NAME, activitiModel.getName());
+            properties.put(ActivitiModelConst.PROPERTIES_PROCESS_ID, workflowModel.getKey());
+            properties.put(ActivitiModelConst.NAME, workflowModel.getName());
             properties.put(ActivitiModelConst.PROPERTIES_PROCESS_AUTHOR, SessionUtil.getCurrentUser().getNickname());
-            properties.put(ActivitiModelConst.PROPERTIES_DOCUMENTATION, activitiModel.getDescription());
+            properties.put(ActivitiModelConst.PROPERTIES_DOCUMENTATION, workflowModel.getDescription());
 
             ObjectNode editorNode = objectMapper.createObjectNode();
             editorNode.put(ActivitiModelConst.ID, "canvas");
@@ -232,8 +224,8 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
     }
 
     @Override
-    public void saveModel(String id, String name, String description, String jsonXml, String svgXml) {
-        org.activiti.engine.repository.Model model = repositoryService.getModel(id);
+    public void saveModel(String id, String name, String description, String jsonXml) {
+        Model model = repositoryService.getModel(id);
         if (model == null) {
             throw new EasyException("获取模型信息失败[modelId = " + id + "]");
         }
@@ -243,31 +235,31 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
         } catch (IOException e) {
             throw new EasyException("获取模型MetaInfo失败");
         }
-        modelJson.put(MODEL_NAME, name);
-        modelJson.put(MODEL_DESCRIPTION, description);
+        modelJson.put(ModelDataJsonConstants.MODEL_NAME, name);
+        modelJson.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
         model.setMetaInfo(modelJson.toString());
         model.setName(name);
         model.setVersion(model.getVersion() + 1);
         // 保存
         repositoryService.saveModel(model);
-        try {
-            repositoryService.addModelEditorSource(model.getId(), jsonXml.getBytes(StandardCharsets.UTF_8));
-            InputStream svgStream = new ByteArrayInputStream(svgXml.getBytes(StandardCharsets.UTF_8));
-            TranscoderInput input = new TranscoderInput(svgStream);
+        //try {
+        repositoryService.addModelEditorSource(model.getId(), jsonXml.getBytes(StandardCharsets.UTF_8));
+        //InputStream svgStream = new ByteArrayInputStream(svgXml.getBytes(StandardCharsets.UTF_8));
+        //TranscoderInput input = new TranscoderInput(svgStream);
+        //
+        //PNGTranscoder transcoder = new PNGTranscoder();
+        //ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        //TranscoderOutput output = new TranscoderOutput(outStream);
+        //
+        //transcoder.transcode(input, output);
+        //final byte[] result = outStream.toByteArray();
+        //repositoryService.addModelEditorSourceExtra(model.getId(), result);
+        //outStream.close();
 
-            PNGTranscoder transcoder = new PNGTranscoder();
-            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-            TranscoderOutput output = new TranscoderOutput(outStream);
-
-            transcoder.transcode(input, output);
-            final byte[] result = outStream.toByteArray();
-            repositoryService.addModelEditorSourceExtra(model.getId(), result);
-            outStream.close();
-
-        } catch (IOException | TranscoderException e) {
-            log.info("保存模型信息失败", e);
-            throw new EasyException(e.getMessage());
-        }
+        //} catch (IOException | TranscoderException e) {
+        //    log.info("保存模型信息失败", e);
+        //    throw new EasyException(e.getMessage());
+        //}
     }
 
     @Override
@@ -316,8 +308,7 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
         byte[] modelBytes = new BpmnXMLConverter().convertToXML(bpmnModel);
         // 部署流程
         String processName = model.getKey() + ".bpmn20.xml";
-        // todo: 这里设置的name没有生效
-        Deployment deployment = repositoryService.createDeployment().name(model.getName())
+        Deployment deployment = repositoryService.createDeployment().key(model.getKey()).name(model.getName())
                 .addString(processName, new String(modelBytes, StandardCharsets.UTF_8))
                 .deploy();
         // 设置模型关联的流程id
@@ -328,7 +319,7 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
 
     @Override
     public boolean checkKeyIsExistence(String key, String id) {
-        QueryWrapper<ActivitiModel> queryWrapper = new QueryWrapper<>();
+        QueryWrapper<WorkflowModelVO> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("key_", key);
         if (StrUtil.isNotBlank(id)) {
             queryWrapper.ne("id_", id);
@@ -338,7 +329,7 @@ public class ActivitiModelServiceImpl extends ServiceImpl<ActivitiModelMapper, A
 
     @Override
     public String selectProcessDefinitionId(String key) {
-        String processDefinitionId = baseMapper.selectProcessDefinitionId(key, ActivitiSuspensionStatus.ACTIVATION.getCode());
+        String processDefinitionId = baseMapper.selectProcessDefinitionId(key, WorkflowSuspensionStatus.ACTIVATION.getCode());
         if (StrUtil.isBlank(processDefinitionId)) {
             throw new EasyException("流程不存在或未部署，请联系系统管理员");
         }
